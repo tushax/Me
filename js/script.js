da26a50b3384c78d99b9a0c2294634d1ab9e1855
tushax <tushrsood@gmail.com>
2015-06-11 19:26:16 +0530

    Angular samples added

--- js/script.js
+++ js/script.js
@@ -105,169 +105,169 @@ var appMaster = {
 
     maps: function(){
         // When the window has finished loading create our google map below
-        google.maps.event.addDomListener(window, 'load', init);
-        google.maps.event.addDomListener(window, 'resize', init);
+//        google.maps.event.addDomListener(window, 'load', init);
+//        google.maps.event.addDomListener(window, 'resize', init);
 
-        function init() {
-            // Basic options for a simple Google Map
-            // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-            var mapOptions = {
-                zoom: 13,
-                draggable: false,
-                zoomControl: true, 
-                scrollwheel:false,
-                streetViewControl:false,
+//        function init() {
+//            // Basic options for a simple Google Map
+//            // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+//            var mapOptions = {
+//                zoom: 13,
+//                draggable: false,
+//                zoomControl: true, 
+//                scrollwheel:false,
+//                streetViewControl:false,
 
-                // The latitude and longitude to center the map (always required)
-                center: new google.maps.LatLng(40.869108,-73.892609), // New York
+//                // The latitude and longitude to center the map (always required)
+//                center: new google.maps.LatLng(40.869108,-73.892609), // New York
 
-                // How you would like to style the map. 
-                // This is where you would paste any style found on Snazzy Maps.
-                styles: [
-                    {
-                        "featureType": "landscape",
-                        "stylers": [
-                            {
-                                "saturation": -100
-                            },
-                            {
-                                "lightness": 65
-                            },
-                            {
-                                "visibility": "on"
-                            }
-                        ]
-                    },
-                    {
-                        "featureType": "poi",
-                        "stylers": [
-                            {
-                                "saturation": -100
-                            },
-                            {
-                                "lightness": 51
-                            },
-                            {
-                                "visibility": "simplified"
-                            }
-                        ]
-                    },
-                    {
-                        "featureType": "road.highway",
-                        "stylers": [
-                            {
-                                "saturation": -100
-                            },
-                            {
-                                "visibility": "simplified"
-                            }
-                        ]
-                    },
-                    {
-                        "featureType": "road.arterial",
-                        "stylers": [
-                            {
-                                "saturation": -100
-                            },
-                            {
-                                "lightness": 30
-                            },
-                            {
-                                "visibility": "on"
-                            }
-                        ]
-                    },
-                    {
-                        "featureType": "road.local",
-                        "stylers": [
-                            {
-                                "saturation": -100
-                            },
-                            {
-                                "lightness": 40
-                            },
-                            {
-                                "visibility": "on"
-                            }
-                        ]
-                    },
-                    {
-                        "featureType": "transit",
-                        "stylers": [
-                            {
-                                "saturation": -100
-                            },
-                            {
-                                "visibility": "simplified"
-                            }
-                        ]
-                    },
-                    {
-                        "featureType": "administrative.province",
-                        "stylers": [
-                            {
-                                "visibility": "off"
-                            }
-                        ]
-                    },
-                    {
-                        "featureType": "water",
-                        "elementType": "labels",
-                        "stylers": [
-                            {
-                                "visibility": "on"
-                            },
-                            {
-                                "lightness": -25
-                            },
-                            {
-                                "saturation": -100
-                            }
-                        ]
-                    },
-                    {
-                        "featureType": "water",
-                        "elementType": "geometry",
-                        "stylers": [
-                            {
-                                "hue": "#ffff00"
-                            },
-                            {
-                                "lightness": -25
-                            },
-                            {
-                                "saturation": -97
-                            }
-                        ]
-                    }
-                ]
-            };
+//                // How you would like to style the map. 
+//                // This is where you would paste any style found on Snazzy Maps.
+//                styles: [
+//                    {
+//                        "featureType": "landscape",
+//                        "stylers": [
+//                            {
+//                                "saturation": -100
+//                            },
+//                            {
+//                                "lightness": 65
+//                            },
+//                            {
+//                                "visibility": "on"
+//                            }
+//                        ]
+//                    },
+//                    {
+//                        "featureType": "poi",
+//                        "stylers": [
+//                            {
+//                                "saturation": -100
+//                            },
+//                            {
+//                                "lightness": 51
+//                            },
+//                            {
+//                                "visibility": "simplified"
+//                            }
+//                        ]
+//                    },
+//                    {
+//                        "featureType": "road.highway",
+//                        "stylers": [
+//                            {
+//                                "saturation": -100
+//                            },
+//                            {
+//                                "visibility": "simplified"
+//                            }
+//                        ]
+//                    },
+//                    {
+//                        "featureType": "road.arterial",
+//                        "stylers": [
+//                            {
+//                                "saturation": -100
+//                            },
+//                            {
+//                                "lightness": 30
+//                            },
+//                            {
+//                                "visibility": "on"
+//                            }
+//                        ]
+//                    },
+//                    {
+//                        "featureType": "road.local",
+//                        "stylers": [
+//                            {
+//                                "saturation": -100
+//                            },
+//                            {
+//                                "lightness": 40
+//                            },
+//                            {
+//                                "visibility": "on"
+//                            }
+//                        ]
+//                    },
+//                    {
+//                        "featureType": "transit",
+//                        "stylers": [
+//                            {
+//                                "saturation": -100
+//                            },
+//                            {
+//                                "visibility": "simplified"
+//                            }
+//                        ]
+//                    },
+//                    {
+//                        "featureType": "administrative.province",
+//                        "stylers": [
+//                            {
+//                                "visibility": "off"
+//                            }
+//                        ]
+//                    },
+//                    {
+//                        "featureType": "water",
+//                        "elementType": "labels",
+//                        "stylers": [
+//                            {
+//                                "visibility": "on"
+//                            },
+//                            {
+//                                "lightness": -25
+//                            },
+//                            {
+//                                "saturation": -100
+//                            }
+//                        ]
+//                    },
+//                    {
+//                        "featureType": "water",
+//                        "elementType": "geometry",
+//                        "stylers": [
+//                            {
+//                                "hue": "#ffff00"
+//                            },
+//                            {
+//                                "lightness": -25
+//                            },
+//                            {
+//                                "saturation": -97
+//                            }
+//                        ]
+//                    }
+//                ]
+//            };
 
-            // Get the HTML DOM element that will contain your map 
-            // We are using a div with id="map" seen below in the <body>
-            var mapElement = document.getElementById('map');
+//            // Get the HTML DOM element that will contain your map 
+//            // We are using a div with id="map" seen below in the <body>
+//            var mapElement = document.getElementById('map');
 
-            // Create the Google Map using out element and options defined above
-            var map = new google.maps.Map(mapElement, mapOptions);
-            var myLatlng = new google.maps.LatLng(40.869108,-73.892609);
-            var image = {
-                url: '../img/map_pin.png',
-                // This marker is 20 pixels wide by 32 pixels tall.
-                size: new google.maps.Size(27, 42),
-                // The origin for this image is 0,0.
-                origin: new google.maps.Point(0, 0),
-                // The anchor for this image is the base of the flagpole at 0,32.
-                anchor: new google.maps.Point(0, 32)
-            };
+//            // Create the Google Map using out element and options defined above
+//            var map = new google.maps.Map(mapElement, mapOptions);
+//            var myLatlng = new google.maps.LatLng(40.869108,-73.892609);
+//            var image = {
+//                url: '../img/map_pin.png',
+//                // This marker is 20 pixels wide by 32 pixels tall.
+//                size: new google.maps.Size(27, 42),
+//                // The origin for this image is 0,0.
+//                origin: new google.maps.Point(0, 0),
+//                // The anchor for this image is the base of the flagpole at 0,32.
+//                anchor: new google.maps.Point(0, 32)
+//            };
 
-            var marker = new google.maps.Marker({
-                position: myLatlng,
-                icon: image,
-                map: map,
-                title: 'Hello World!'
-            });
+//            var marker = new google.maps.Marker({
+//                position: myLatlng,
+//                icon: image,
+//                map: map,
+//                title: 'Hello World!'
+//            });
 
-        }
+//        }
     },
 
     isoTop: function(){
